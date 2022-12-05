ccbea80bacf38442f372628f6e00a118d8f59525
NikolaGadze <85805336+NikolaGadze@users.noreply.github.com>
2022-12-05 01:32:24 +0100

    Prvi commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-app id="inspire">
+
+    <v-app-bar app class="blue-grey darken-4">
+      <v-app-bar-nav-icon style="color: white" @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title style="color: white">Aplikacija za pretragu instrukcija</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      color="blue-grey darken-4"
+      v-model="drawer"
+      fixed
+      temporary
+    >
+    <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link"
+          :to="link.ruta"
+          link
+          color="light-blue"       
+        >
+          <v-list-item-content>
+            <v-list-item-title style="color: white;">{{ link.ime }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+
+    <v-main class="blue-grey darken-4">
+      <v-container fluid>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="12">
+            <router-view/>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+      <v-footer
+        color="primary"
+        padless
+      >
+        <v-row
+          justify="center"
+          no-gutters
+        >
+          <v-col
+            class="deep-purple darken-4 py-4 text-center white--text"
+            cols="12"
+          >
+            {{ new Date().getFullYear() }} — <strong>Ivan Raič i Nikola Gadže</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
+
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      cards: ['Today', 'Yesterday'],
+      drawer: null,
+      links: [
+        {ime: 'Pocetna', ruta: '/'},
+      ],
+    }),
+  }
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,241 @@
+<template>
+  <div class="home">
+    <v-container fluid>
+
+      <v-row style="text-align: center">
+          <v-col cols="12">
+            <a href="./Projekt.pdf" target="_blank" style="color: dodgerblue; text-decoration: none; font-size: x-large;">Vizija</a>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12">
+            <v-divider color="purple"></v-divider>
+          </v-col>
+        </v-row>
+
+      <v-row style="color:white; font-size: x-large"> 
+        <v-col cols="12">
+          <h2 style="text-align:center; margin-bottom: 10px;">Osnovno o projektu</h2>
+          <p>
+            Naziv projekta je Aplikacija za pretragu instrukcija, čiji je osnovni cilj studentima i učenicima omogućiti
+             što lakše stupanje u kontakt s predavačima koji nude usluge instrukcija, te koji se također kao korisnici prijavljuju na sustav.
+             Ulogu gosta u ovoj aplikaciji imaju učenici i studenti, kao i sve ostale osobe koje nisu prijavljene na sustav te oni mogu pregledavati
+             sadržaje na aplikaciji, te tako doći do potebnih informacija koje im trebaju broj telefona, email, mjesto i slično.
+             Ulogu korisnika imaju učenici i studenti koji su prijavljeni na sustav. Oni osim što mogu vidjeti potrebne informacije oni direktno mogu stupiti u kontakt
+             sa predavačom preko aplikacije, i zabilježiti sebi mjesto, ako su im zadovoljeni ponuđenim predavačom, te su također u mogućnosti davati recenzije predavačima.
+             Predavači također imaju ulogu korisnika, ali oni mogu objavljivati i uređivati svoje oglase i raspored. Administrator pregledava rad sustava i upravlja korisnicima.
+          </p>
+        </v-col>
+      </v-row>
+
+
+      <v-row>
+          <v-col cols="12">
+            <v-divider color="purple"></v-divider>
+          </v-col>
+        </v-row>
+
+      <v-row style="color:white; font-size: x-large"> 
+        <v-col cols="12">
+          <h2 style="text-align:center; margin-bottom: 10px;">Motivacija za projekt</h2>
+          <p>
+           Motivaciju za ovaj projekt smo dobili jer smo i mi studenti i sami nekada imamo potrebu za pronalazak instrukcija.
+            S obzirom na malu dostupnost predavača, te malu dostupnost provjernih informacija teško je stupiti u kontakt s dobrim i provjerenim predavačima.
+            Ovim projektom želimo olakšati pronalazak dobrih i dostupnih predavača po željenim parametrima.
+          </p>
+        </v-col>
+      </v-row>
+
+      <v-row>
+          <v-col cols="12">
+            <v-divider color="purple"></v-divider>
+          </v-col>
+        </v-row>
+
+
+        <v-row style="text-align: center">
+          <v-col cols="12">
+            <h2 style="color: white; font-size: 30px">Informacije o autorima projekta</h2>
+          </v-col>
+        </v-row>
+
+
+        <v-row>
+          <v-col cols="6" style="color: white; text-align: center; font-size: x-large;">
+            <div>
+              <v-icon color="white" size="30px" @click="toGithubIvan()">mdi-github</v-icon>
+              <h2>
+                Ivan Raič
+              </h2>
+              <p style="text-align: center">Ivan Raič, rođen 11.11.2001 godine u Mostaru,<br>
+                rodom iz Stoca, a živi u okolici Stoca.<br>
+                Završio Osnovnu školu Stolac,<br>
+                a zatim u Srednjoj Strukovnoj Školi Stolac,<br>
+                završio smjer Računalni tehničar za strojarstvo<br>
+                Nakon toga upisuje Informatiku na FPMOZ-u<br>
+                gdje je trenutno student 3.godine.
+              </p>
+
+            </div>
+          </v-col>
+
+
+          <v-col cols="6" style="color: white; text-align: center; font-size: x-large">
+            <div>
+              <v-icon color="white" size="30px" @click="toGithubNikola()">mdi-github</v-icon>
+              <h2>
+                Nikola Gadže
+              </h2>
+              <p style="text-align: center">Nikola Gadže, rođen 9.11.2001 godine u Splitu,<br>
+                rodom iz Ljubuškog, a živi u okolici Ljubuškog.<br>
+                Završio Osnovnu školu Tina Ujevića u Vitini,<br>
+                a zatim Gimnaziju u Ljubuškom.<br>
+                Nakon toga upisuje Informatiku na FPMOZ-u<br>
+                gdje je trenutno student 3.godine.
+              </p>
+            </div>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="6">
+            <template>
+              <v-row justify="center">
+                <v-dialog
+                  v-model="dialog1"
+                  width="600px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      color="deep-purple darken-4"
+                      dark
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      Ivan Raič
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    
+                    <v-card-text>
+                      <br>
+                      <v-img :src="require('../assets/IvanRaic.jpeg')" height="630" width="550" ></v-img>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer> 
+                      
+                      <v-btn
+                        color="red darken-1"
+                        text
+                        @click="dialog1 = false"
+                      >
+                        Zatvori
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-row>
+            </template>
+          </v-col>
+
+
+          <v-col cols="6">
+            <template>
+              <v-row justify="center">
+                <v-dialog
+                  v-model="dialog2"
+                  width="600px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      color="deep-purple darken-4"
+                      dark
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      Nikola Gadže
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    
+                    <v-card-text>
+                      <br>
+                      <v-img :src="require('../assets/NikolaGadze.jpeg')" height="630" width="550" ></v-img>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer> 
+                      
+                      <v-btn
+                        color="red darken-1"
+                        text
+                        @click="dialog2 = false"
+                      >
+                        Zatvori
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-row>
+            </template>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12">
+            <v-divider color="purple"></v-divider>
+          </v-col>
+        </v-row>
+
+        <v-row style="text-align: center">
+          <v-col cols="12">
+            <h2 style="color: white; font-size: 30px">Tehnologije koje će se koristiti za izradu aplikacije</h2>
+          </v-col>
+        </v-row>
+
+        <v-row style="text-align:center; color:aliceblue">
+          <v-col v-for="tehnologija in tehnologije" :key="tehnologija" cols="12" xl="4" lg="4" md="6" sm="12" xs="12">
+            <v-img
+            height="350"
+            :src="tehnologija.slika">
+
+            </v-img>
+            <p>{{ tehnologija.ime }}</p>
+          </v-col>
+        </v-row>
+      </v-container>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  data: () => ({
+    dialog1: false,
+    dialog2: false,
+  tehnologije : [
+    {ime: 'HTML', slika: 'https://imgs.search.brave.com/tNr6FaOC1uPTCjt0rtO71COF0rJYMdum0rjC-Dt3NQc/rs:fit:600:350:1/g:ce/aHR0cHM6Ly93d3cu/ZnJlZXBuZ2xvZ29z/LmNvbS91cGxvYWRz/L2h0bWw1LWxvZ28t/cG5nL2h0bWw1LWxv/Z28taW1hZ2UtbG9n/by1odG1sLTcucG5n'},
+    {ime: 'CSS', slika: 'https://imgs.search.brave.com/MgT3JX9doCmi_iWfKy11g9HbzQDdI_ml3g4F_NeSB5A/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly8xMDAw/bG9nb3MubmV0L3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDIwLzA5/L0NTUy1Mb2dvLnBu/Zw'},
+    {ime: 'Bootstrap', slika: 'https://imgs.search.brave.com/Ytvcr5-8bzOPaIvNKP4aa6B5bfoUAi1IZUKoekDTgQI/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9kb3du/bG9hZC5sb2dvLndp/bmUvbG9nby9Cb290/c3RyYXBfKGZyb250/LWVuZF9mcmFtZXdv/cmspL0Jvb3RzdHJh/cF8oZnJvbnQtZW5k/X2ZyYW1ld29yaykt/TG9nby53aW5lLnBu/Zw'},
+    {ime: 'JavaScript', slika: 'https://imgs.search.brave.com/zRXPpOhRJEdccUWHHnguELZyqzCuqWQmAjKXmRiv5do/rs:fit:512:512:1/g:ce/aHR0cHM6Ly9sb2dv/ZGl4LmNvbS9sb2dv/LzM3NDk3Mi5wbmc'},
+    {ime: 'VueJS', slika: 'https://imgs.search.brave.com/VnUn-w-q5O0xeIwrPTs_eDI21iDgK71SJoVtjDa7f3Q/rs:fit:924:636:1/g:ce/aHR0cHM6Ly9kd2ds/b2dvLmNvbS93cC1j/b250ZW50L3VwbG9h/ZHMvMjAxNy8wOS9W/dWVfanNfbG9nby5w/bmc'},
+    {ime: 'Vuetify', slika: 'https://imgs.search.brave.com/TKjF7IQj8HpPKvXIbpPUgghJbMYUkN2ExDW-mJ4qH8Q/rs:fit:600:600:1/g:ce/aHR0cHM6Ly9pY29u/YXBlLmNvbS93cC1j/b250ZW50L2ZpbGVz/L25vLzExMzEwMS9w/bmcvdnVldGlmeS5w/bmc'},
+    {ime: 'PHP', slika: 'https://imgs.search.brave.com/pB93qbxyeUYui4r3eL_tz-qQiLSM6kdrzdbRPglIJ7Q/rs:fit:1200:1060:1/g:ce/aHR0cHM6Ly9kYWIx/bm1zbHZ2bnRwLmNs/b3VkZnJvbnQubmV0/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE2/LzA0LzE0NTk4NzAz/MTNQSFAtbG9nby5z/dmcucG5n'},
+    {ime: 'Laravel', slika: 'https://imgs.search.brave.com/YqHCjQS952JzncK5-HTyS5g4IcCf-JV-Yi9Pa2TS7S8/rs:fit:1024:1024:1/g:ce/aHR0cHM6Ly9sb2dv/c3BuZy5vcmcvZG93/bmxvYWQvbGFyYXZl/bC9sb2dvLWxhcmF2/ZWwtMTAyNC5wbmc'},
+    {ime: 'SQL', slika: 'https://imgs.search.brave.com/lIh89F0-ubarUMqSFWwTLETeYIb3rCK4GwTaPugWfWI/rs:fit:600:315:1/g:ce/aHR0cHM6Ly9henVy/ZS5taWNyb3NvZnQu/Y29tL3N2Z2hhbmRs/ZXIvc3FsLXNlcnZl/ci1zdHJldGNoLWRh/dGFiYXNlP3dpZHRo/PTYwMCZoZWlnaHQ9/MzE1'}
+  ]
+  }),
+
+  methods: {
+    toGithubNikola: function(){
+        window.open('https://github.com/NikolaGadze')
+      },
+      toGithubIvan: function(){
+        window.open('https://github.com/raici')
+      },
+  }
+  
+}
+</script>
