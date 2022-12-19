9d3e351e67ad6c98ee73581d5d9c3080c434e504
NikolaGadze <85805336+NikolaGadze@users.noreply.github.com>
2022-12-19 10:36:58 +0100

    Build

---
+++ dist/js/about.6902c005.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkprojekt_naslovna"]=self["webpackChunkprojekt_naslovna"]||[]).push([[443],{9443:function(n,t,u){u.r(t),u.d(t,{default:function(){return o}});var l=function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"})},s=[],a=u(1001),e={},r=(0,a.Z)(e,l,s,!1,null,null,null),o=r.exports}}]);
+//# sourceMappingURL=about.6902c005.js.map
